6972f558fe81a7acd3fd2d3dcbd3b93884ea833d
Ikram Rasheed <ikramrasheed333@gmail.com>
2025-03-19 23:29:31 +0500

    chore: fixed issues

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ module.exports = {
     config.module.rules.push({
       test: /\.worker\.js$/,
       loader: "worker-loader",
-      // options: { inline: true }, // also works
-      options: {
-        name: "static/[hash].worker.js",
-        publicPath: "/_next/",
-      },
+      options: { inline: "fallback" }, // also works
+      //   options: {
+      //     name: "static/[hash].worker.js",
+      //     publicPath: "/_next/",
+      //   },
     });
     return config;
   },
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Example extends Component {
   componentDidMount() {
-    this.worker = new Worker(new URL('../example.worker', import.meta.url));
-    this.worker.postMessage('from Host');
-    this.worker.addEventListener('message', this.onWorkerMessage);
+    this.worker = new Worker(new URL("../example.worker", import.meta.url));
+    this.worker.postMessage("from Host");
+    this.worker.addEventListener("message", this.onWorkerMessage);
   }
   componentWillUnmount() {
     this.worker.terminate();
   }
-  onWorkerMessage = (event) => console.log('Host received:', event.data);
+  onWorkerMessage = (event) => console.log("Host received:", event.data);
   render() {
-    return <h1>Web Worker Example</h1>
+    return <h1>Web Worker Example</h1>;
   }
 }
